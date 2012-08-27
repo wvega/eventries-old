51d21fec0d6a81126bf58124cf5c45b0e1d25271
Willington Vega <wvega@wvega.com>
2012-08-27 00:08:21 -0500

    Update Fabric recipes and DB backup.

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,11 @@
 # coding: utf-8
 
-from fabric.api import env
+from fabric.api import env, run, cd
 
 from skeleton import init, options, utils
 from skeleton.git import git
 from skeleton.httpd import vhost
-from skeleton.mysql import replace, backup
-from skeleton.wordpress import setup, wordpress, prepare, config
+from skeleton.wordpress import setup, wordpress, prepare, config, backup, replace
 
 # RackSpace
 env.user = 'root'
@@ -15,3 +14,24 @@ env.hosts = ['108.166.90.148']
 
 def sync():
     utils.sync('/var/www/html/')
+
+
+def rebuild():
+    mysql = 'mysql -uroot -ppassword'
+
+    with cd('/var/www/html/eventries.com'):
+        run('%s -e "DROP DATABASE IF EXISTS eventries; CREATE DATABASE eventries;"' % mysql)
+        run('%s --database eventries < sql/eventries.com-testing-latest.sql' % mysql)
+
+
+def deploy():
+    # get recent DB backup
+    backup()
+    # switch to testing server wp-config.php file
+    config('testing', True)
+    # upload files
+    sync()
+    # switch back to local wp-config.php file
+    config()
+    # update remote database
+    rebuild()
